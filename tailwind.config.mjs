cdb39c1698f5e5c12f98dec2f2ac255820a4f089
luiz2k <luizteles934@gmail.com>
2024-10-03 10:42:08 -0300

    Preparar ambiente de desenvlvimento

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EB2411",
+        secondary: "#2181EE",
+        accent: "#F6AC07",
+      },
+    },
+  },
+  plugins: [],
+};
